11bb6583c824b5b6855f840021404678ca481176
dmbromb <dmbromb@gmail.com>
2024-07-08 11:53:45 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+def text_to_morse(word, translation_dictionary):
+    morse = ''
+    for letter in word:
+        morse += f'{translation_dictionary[letter.title()]} '
+    return morse
+
+url = 'https://en.wikipedia.org/wiki/Morse_code'
+data = pd.read_html(url)
+df = (data[0])
+df = df[['Character', 'Code']].loc[0:25]
+df['Character'] = (df['Character'].str.split(',', expand=True)).iloc[:, 0]
+df['Code'] = (df['Code'].str.replace('ⓘ', '')).str.replace('\xa0', ' ')
+df = (df.set_index('Character'))
+
+keep_going = True
+
+while keep_going:
+    dict = df.to_dict()['Code']
+    word = input('\nWhat word would you like to translate? \n')
+    print(text_to_morse(word, dict))
+    y_n = input('\nWould you like to translate another word? (y/n)\n')
+    if y_n.upper() != 'Y':
+        keep_going = False
